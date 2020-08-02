9b9924f387ba09d2a5f9cc5e23ecb98b71434916
Samuel Gratzl <sam@sgratzl.com>
2020-08-02 10:51:40 +0200

    small tunings

--- src/stats/categoricalStats.ts
+++ src/stats/categoricalStats.ts
@@ -1,37 +1,40 @@
-import { defaultCategoricalColorScale } from "../renderers/defaults";
-import { ICommonStats } from "./common";
+import { defaultCategoricalColorScale } from '../renderers/defaults';
+import { ICommonStats } from './common';
 
-export interface ICategoricalStats extends ICommonStats<string> {
-}
+export interface ICategoricalStats extends ICommonStats<string> {}
 
 export interface CategoricalStatsOptions {
-    color?: ((v: string) => string);
+  color?: (v: string) => string;
 }
 
 export function categoricalStats(options: CategoricalStatsOptions = {}) {
-    const color = options.color ?? defaultCategoricalColorScale();
+  const color = options.color ?? defaultCategoricalColorScale();
 
-    return (arr: readonly string[]): ICategoricalStats => {
-        let missing = 0;
-        const map = new Map<string, number>();
-        for (const v of arr) {
-            if (v == null) {
-                missing++;
-                continue;
-            }
-            map.set(v, 1 + (map.get(v) ?? 0));
-        }
-        const hist = Array.from(map.entries()).sort((a, b) => a[0].localeCompare(b[0])).map(([value, count]) => ({
-            x0: value, x1: value, count,
-            color: color(value)
-        }));
-        return {
-            hist,
-            maxBin: hist.reduce((acc, b) => Math.max(acc, b.count), 0),
-            color,
-            format: (v) => v,
-            missing,
-            count: arr.length
-        }
+  return (arr: readonly string[]): ICategoricalStats => {
+    let missing = 0;
+    const map = new Map<string, number>();
+    for (const v of arr) {
+      if (v == null) {
+        missing++;
+        continue;
+      }
+      map.set(v, 1 + (map.get(v) ?? 0));
     }
+    const hist = Array.from(map.entries())
+      .sort((a, b) => a[0].localeCompare(b[0]))
+      .map(([value, count]) => ({
+        x0: value,
+        x1: value,
+        count,
+        color: color(value),
+      }));
+    return {
+      hist,
+      maxBin: hist.reduce((acc, b) => Math.max(acc, b.count), 0),
+      color,
+      format: (v) => v,
+      missing,
+      count: arr.length,
+    };
+  };
 }
