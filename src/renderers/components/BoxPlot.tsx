5b5e1bca9d5a5190f7cbbfa01d261385aae362a4
Samuel Gratzl <sam@sgratzl.com>
2020-08-16 19:27:24 +0200

    add unfiltered pre boxplot

--- src/renderers/components/BoxPlot.tsx
+++ src/renderers/components/BoxPlot.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { INumberStats } from '../../math/numberStatsGenerator';
 import './BoxPlot.css';
 import './Summary.css';
+import { IBoxPlot } from '@sgratzl/boxplots';
 
 export interface BoxPlotProps {
-  s: INumberStats;
+  s: INumberStats & { preFilter?: IBoxPlot };
   className?: string;
   style?: React.CSSProperties;
 }
 
-function generatePath(stats: INumberStats, s: (v: number) => number, h: number, padding: number) {
+function generatePath(stats: IBoxPlot, s: (v: number) => number, h: number, padding: number) {
   const cy = h / 2;
   const q1 = s(stats.q1);
   const q3 = s(stats.q3);
@@ -21,50 +22,71 @@ function generatePath(stats: INumberStats, s: (v: number) => number, h: number, 
   return `${whiskerLow} ${whiskerHigh} ${median}`;
 }
 
-function generateTitle(s: INumberStats) {
-  return `Minimum: ${s.format(s.min)}
-25% Quantile: ${s.format(s.q1)}
-Median: ${s.format(s.median)}
-Mean: ${s.format(s.mean)}
-75% Quantile: ${s.format(s.q3)}
-Maximum: ${s.format(s.max)}
-# Items: ${s.count.toLocaleString()}`;
+function generateTitle(s: INumberStats, pre?: IBoxPlot) {
+  const p = (v: number) => `/${s.format(v)}`;
+  return `Minimum: ${s.format(s.min)}${pre ? p(pre.min) : ''}
+25% Quantile: ${s.format(s.q1)}${pre ? p(pre.q1) : ''}
+Median: ${s.format(s.median)}${pre ? p(pre.median) : ''}
+Mean: ${s.format(s.mean)}${pre ? p(pre.mean) : ''}
+75% Quantile: ${s.format(s.q3)}${pre ? p(pre.q3) : ''}
+Maximum: ${s.format(s.max)}${pre ? p(pre.max) : ''}
+# Items: ${s.count.toLocaleString()}${pre ? `/${pre.count.toLocaleString()}` : ''}`;
 }
 
 export default function BoxPlot(props: BoxPlotProps) {
   const s = props.s;
-  const height = 20;
-  const cy = height / 2;
+  const pre = s.preFilter;
   const boxPadding = 2;
   const scale = (v: number) => Math.round(s.scale(v) * 1000) / 10;
   const outlierRadius = 4;
-  const path = generatePath(s, scale, height, boxPadding);
-  const title = generateTitle(s);
+  const height = 20;
+
+  const generateBoxPlot = (b: IBoxPlot) => {
+    const cy = height / 2;
+    const path = generatePath(b, scale, height, boxPadding);
+    return (
+      <>
+        <rect
+          x={scale(b.q1)}
+          y={boxPadding}
+          height={height - boxPadding}
+          width={scale(b.median) - scale(b.q1)}
+          className="lt-boxplot-box"
+          style={{ fill: s.color(s.scale((b.median + b.q1) / 2)) }}
+        />
+        <rect
+          x={scale(b.median)}
+          y={boxPadding}
+          height={height - boxPadding}
+          width={scale(b.q3) - scale(b.median)}
+          style={{ fill: s.color(s.scale((b.median + b.q3) / 2)) }}
+          className="lt-boxplot-box"
+        />
+        <path d={path} className="lt-boxplot-frame" />
+        {b.outlier.map((o) => (
+          <circle className="lt-boxplot-outlier" key={o} cx={scale(o)} cy={cy} r={outlierRadius}>
+            <title>{s.format(o)}</title>
+          </circle>
+        ))}
+      </>
+    );
+  };
+
+  const offset = pre != null ? height * 0.1 : 0;
   return (
-    <svg viewBox={`0 0 100 ${height}`} className={props.className} style={props.style} preserveAspectRatio="xMaxYMid">
-      <title>{title}</title>
-      <rect
-        x={scale(s.q1)}
-        y={boxPadding}
-        height={height - boxPadding}
-        width={scale(s.median) - scale(s.q1)}
-        className="lt-boxplot-box"
-        style={{ fill: s.color(s.scale((s.median + s.q1) / 2)) }}
-      />
-      <rect
-        x={scale(s.median)}
-        y={boxPadding}
-        height={height - boxPadding}
-        width={scale(s.q3) - scale(s.median)}
-        style={{ fill: s.color(s.scale((s.median + s.q3) / 2)) }}
-        className="lt-boxplot-box"
-      />
-      <path d={path} className="lt-boxplot-frame" />
-      {s.outlier.map((o) => (
-        <circle className="lt-boxplot-outlier" key={o} cx={scale(o)} cy={cy} r={outlierRadius}>
-          <title>{s.format(o)}</title>
-        </circle>
-      ))}
+    <svg
+      viewBox={`0 0 100 ${height + offset}`}
+      className={props.className}
+      style={props.style}
+      preserveAspectRatio="xMaxYMid"
+    >
+      <title>{generateTitle(s, pre)}</title>
+      {pre != null && pre.count > s.count && (
+        <g className="lt-boxplot-pre" transform={`translate(0,${offset})`}>
+          {generateBoxPlot(pre)}
+        </g>
+      )}
+      {generateBoxPlot(s)}
     </svg>
   );
 }
