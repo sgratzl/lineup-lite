240a5e11c73d4b6e9808b9df9598922385b3baaa
Samuel Gratzl <sam@sgratzl.com>
2020-08-27 16:03:46 +0200

    dummy test

--- packages/hooks/src/renderers/BarRenderer.test.tsx
+++ packages/hooks/src/renderers/BarRenderer.test.tsx
@@ -1,12 +1,6 @@
 /// <reference types="jest" />
 import '../setupTests';
-import React from 'react';
-import { render } from '@testing-library/react';
-import { BarRenderer } from './BarRenderer';
 
-test('renders learn react link', () => {
-  const f =
-  const { getByText } = render(<BarRenderer />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('dummy', () => {
+  expect(true).toBeTruthy();
 });
