5c1df1d413b3209b138dc84e25da4c8536b0b961
Samuel Gratzl <sam@sgratzl.com>
2020-08-30 17:26:45 +0200

    import fixes

--- packages/components/src/bundle.ts
+++ packages/components/src/bundle.ts
@@ -1,3 +1,12 @@
-import './styles.css';
+import './components/BoxPlot.css';
+import './components/CategoricalColor.css';
+import './components/DateLabel.css';
+import './components/FilterRange.css';
+import './components/Histogram.css';
+import './components/NumberBar.css';
+import './components/NumberColor.css';
+import './components/NumberSymbol.css';
+import './components/TextSummary.css';
+import './components/Summary.css';
 
 export * from '.';
