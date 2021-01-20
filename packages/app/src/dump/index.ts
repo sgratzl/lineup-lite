f2e7ad4b18e0d03d3a2eafcb236b4d53315f13fa
Samuel Gratzl <sam@sgratzl.com>
2021-01-20 19:36:31 +0100

    feat: remove app in favor of docs

--- packages/app/src/dump/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @lineup-lite/app
- * https://github.com/sgratzl/lineup-lite
- *
- * Copyright (c) 2021 Samuel Gratzl <sam@sgratzl.com>
- */
-
-export { default as loadFile, loadJSON } from './loadFile';
--- packages/app/tsd.d.ts
+++
@@ -1,3 +0,0 @@
-//allow file dependencies
-declare module '!file-loader!*';
-declare module '!raw-loader!*';
