24a93affbd56ee8b13d10e38340d8bc044eff0f5
Samuel Gratzl <sam@sgratzl.com>
2021-01-30 17:15:00 +0100

    feat: simple upset line

--- examples/components/src/App.tsx
+++ examples/components/src/App.tsx
@@ -8,8 +8,13 @@ export default function App() {
   return (
     <div className="App">
       Hello {isDarkTheme}
-      <div style={{ display: 'flex', height: 100 }}>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
         <UpSetLine value={['a', 'b']} sets={['a', 'b', 'c']} style={{ flex: '1 1 0' }} />
+        <UpSetLine value={['a', 'c']} sets={['a', 'b', 'c']} style={{ flex: '1 1 0' }} />
+        <UpSetLine value={['a']} sets={['a', 'b', 'c']} style={{ flex: '1 1 0' }} />
+        <UpSetLine value={['b']} sets={['a', 'b', 'c']} style={{ flex: '1 1 0' }} />
+        <UpSetLine value={['b', 'c']} sets={['a', 'b', 'c']} style={{ flex: '1 1 0' }} />
+        <UpSetLine value={['b', 'c', 'g']} sets={['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']} style={{ flex: '1 1 0' }} />
       </div>
     </div>
   );
--- packages/components/src/components/UpSetLine.tsx
+++ packages/components/src/components/UpSetLine.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { CommonProps } from './common';
-import { clsx, format } from './utils';
+import { clsx, format, toPercent } from './utils';
 
 export interface UpSetLineProps extends CommonProps {
   /**
@@ -17,15 +17,30 @@ export interface UpSetLineProps extends CommonProps {
 
 export function UpSetLine(props: UpSetLineProps) {
   const has = props.value instanceof Set ? props.value : new Set(props.value);
+  const first = props.sets.findIndex((d) => has.has(d));
+  let last = first;
+  if (has.size > 1 && first < props.sets.length - 1) {
+    last =
+      props.sets.length -
+      1 -
+      props.sets
+        .slice()
+        .reverse()
+        .findIndex((d) => has.has(d));
+  }
   return (
     <div className={clsx('lt-upset-line', props.className)} style={props.style}>
       {props.sets.map((s) => (
-        <div
-          key={s}
-          className={clsx('lt-upset-dot', has.has(s) && 'lt-upset-dot-enabled')}
-          title={format(s, props.format)}
-        />
+        <svg key={s} className={clsx('lt-upset-dot', has.has(s) && 'lt-upset-dot-enabled')} viewBox="0 0 2 2">
+          <title>{format(s, props.format)}</title>
+          <circle r="1" cx="1" cy="1" />
+        </svg>
       ))}
+      {first < last && (
+        <svg className="lt-upset-line-line" viewBox={`0 0 ${props.sets.length * 2} 2`}>
+          <line x1={first * 2 + 1} x2={last * 2 + 1} y1={1} y2={1} />
+        </svg>
+      )}
     </div>
   );
 }
