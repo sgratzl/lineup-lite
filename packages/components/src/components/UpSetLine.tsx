0a6f8aefe7f79bc8a2712659e4c44290d81d42c0
Samuel Gratzl <sam@sgratzl.com>
2021-04-07 09:31:59 -0400

    fix: upsetline not visible in firefox

--- packages/components/src/components/UpSetLine.tsx
+++ packages/components/src/components/UpSetLine.tsx
@@ -50,13 +50,13 @@ export interface UpSetLineProps extends CommonProps {
   i18n?: Partial<typeof UPSET_LINE_I18N_EN>;
 }
 
-function generateGradient(colors: string[]) {
+function generateGradient(colors: string[], x1: number, x2: number) {
   const id = `upset-line-g-${colors.join(',')}`.replace(/[, ]+/gm, '').replace(/[-#$()[\]{}"']+/gm, '-');
   return {
     stroke: `url('#${id}')`,
     def: (
       <defs>
-        <linearGradient id={id}>
+        <linearGradient id={id} x1={x1} x2={x2} gradientUnits="userSpaceOnUse">
           {colors.map((d, i) => (
             <stop key={d} offset={toPercent(i / (colors.length - 1))} stopColor={d} />
           ))}
@@ -86,7 +86,7 @@ function UpSetLineLine({
       // eslint-disable-next-line prefer-destructuring
       stroke = colors[0];
     } else {
-      const r = generateGradient(colors);
+      const r = generateGradient(colors, first * 2, last * 2);
       stroke = r.stroke;
       g = r.def;
     }
